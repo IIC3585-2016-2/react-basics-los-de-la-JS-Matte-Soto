2c6664de58d1b87deebab5669ba0fbe95e84731d
Sebastián Oliva <saoliva@uc.cl>
2016-10-05 13:21:54 -0300

    add react-router example

---
+++ RoutingReact/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+import { Router, Route, browserHistory} from 'react-router'
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      //<Route path="home" component={Home}/>
+      <Route path="/repos" component={Repos}>
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
---
+++ RoutingReact/modules/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+      <h1> Hola estoy en Home </h1>
+
+      </div>
+
+
+    )
+  }
+})
